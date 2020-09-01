4bbfd31f599e9795fa0b9a8ad30f668fe9c94daa
jakeogh <github.com@v6y.net>
2020-08-31 23:04:29 -0700

    auto-commit

--- pubchemmer/pubchemmer.py
+++ pubchemmer/pubchemmer.py
@@ -30,7 +30,7 @@ from kcl.configops import click_read_config
 from kcl.configops import click_write_config_entry
 from kcl.inputops import enumerate_input
 from pubchemmer.sdf_field_types import SDF_FIELD_TYPES
-##from pubchemmer.PubChem import PubChem
+from pubchemmer.PubChem import PubChem
 from sqlalchemy_utils.functions import create_database
 from kcl.sqlalchemy.self_contained_session import self_contained_session
 from kcl.sqlalchemy.delete_database import delete_database as really_delete_database
