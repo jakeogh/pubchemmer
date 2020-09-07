8c8b443423ec1e33d36ee49c5fbe08c3bc39b1c5
jakeogh <github.com@v6y.net>
2020-09-06 20:16:07 -0700

    auto-commit

--- pubchemmer/pubchemmer.py
+++ pubchemmer/pubchemmer.py
@@ -299,6 +299,34 @@ def last_cid(verbose,
 
 
 @cli.command()
+@click.option('--verbose', is_flag=True)
+@click.option('--debug', is_flag=True)
+@click.option('--ipython', is_flag=True)
+def describe(verbose,
+             debug,
+             ipython):
+
+    global APP_NAME
+    database = 'postgres://postgres@localhost/' + APP_NAME
+
+    config, config_mtime = click_read_config(click_instance=click,
+                                             app_name=APP_NAME,
+                                             verbose=verbose)
+    if verbose:
+        ic(config, config_mtime)
+
+    #query = "SELECT pubchem.pubchem_compound_cid from pubchem ORDER BY pubchem.pubchem_compound_cid"
+    query = "select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = 'pubchem';"
+
+    with self_contained_session(db_url=database) as session:
+        for index, match in enumerate(session.bind.execute(query).fetchall()):
+            ic(index, match)
+
+        if ipython:
+            import IPython; IPython.embed()
+
+
+@cli.command()
 @click.argument('match', type=str, nargs=1)
 @click.option('--verbose', is_flag=True)
 @click.option('--cid', is_flag=True)
