c38a7b6da73cdd3c5f35a98fdd4898e1eacf5ee9
jakeogh <github.com@v6y.net>
2024-08-15 02:17:13 -0700

    auto-commit

--- pubchemmer/pubchemmer.py
+++ pubchemmer/pubchemmer.py
@@ -478,7 +478,6 @@ def describe(
     )
     ic(config, config_mtime)
 
-    # query = "SELECT pubchem.pubchem_compound_cid from pubchem ORDER BY pubchem.pubchem_compound_cid"
     query = "select column_name, data_type, character_maximum_length, column_default, is_nullable from INFORMATION_SCHEMA.COLUMNS where table_name = 'pubchem';"
 
     ic("column_name, data_type, character_maximum_length, column_default, is_nullable")
